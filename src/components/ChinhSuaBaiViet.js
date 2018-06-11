91830078aec5a9bb72b08a6a0fe82ff0122630fd
Sang <hoang84119@gmail.com>
2018-06-11 14:30:36 +0700

    fix

--- src/components/ChinhSuaBaiViet.js
+++ src/components/ChinhSuaBaiViet.js
@@ -52,27 +52,29 @@ class CTBaiBao extends Component {
     formData.append("content", content);
     fetch(
       API.getURL() +
-      "/thuctap/wp-json/wp/v2/posts/" +
-      this.props.navigation.getParam("id", ""), {
+        "/thuctap/wp-json/wp/v2/posts/" +
+        this.props.navigation.getParam("id", ""),
+      {
         headers: {
-          'Authorization': 'Basic ' + Base64.btoa('admin:yEgN NbO6 w6k3 vSuU xBjV E8Ok'),
+          Authorization:
+            "Basic " + Base64.btoa("admin:yEgN NbO6 w6k3 vSuU xBjV E8Ok")
         },
         body: formData,
-        method: 'POST',
+        method: "POST"
       }
-    )
-      .then(response => {
-        var t = response.status;
-        if (response.status == "200") Alert.alert("Thông báo", "Đã lưu thành công");
-        else Alert.alert("Lỗi", "Thất bại");
-      });
+    ).then(response => {
+      var t = response.status;
+      if (response.status == "200")
+        Alert.alert("Thông báo", "Đã lưu thành công");
+      else Alert.alert("Lỗi", "Thất bại");
+    });
   }
 
   async loadData() {
     fetch(
       API.getURL() +
-      "/thuctap/wp-json/wp/v2/posts/" +
-      this.props.navigation.getParam("id", "")
+        "/thuctap/wp-json/wp/v2/posts/" +
+        this.props.navigation.getParam("id", "")
     )
       .then(response => response.json())
       .then(responeJson => {
@@ -155,40 +157,47 @@ const myStyle = StyleSheet.create({
   }
 });
 
-const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+const chars =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
 const Base64 = {
-  btoa: (input) => {
+  btoa: input => {
     let str = input;
-    let output = '';
+    let output = "";
 
-    for (let block = 0, charCode, i = 0, map = chars;
-      str.charAt(i | 0) || (map = '=', i % 1);
-      output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+    for (
+      let block = 0, charCode, i = 0, map = chars;
+      str.charAt(i | 0) || ((map = "="), i % 1);
+      output += map.charAt(63 & (block >> (8 - (i % 1) * 8)))
+    ) {
+      charCode = str.charCodeAt((i += 3 / 4));
 
-      charCode = str.charCodeAt(i += 3 / 4);
-
-      if (charCode > 0xFF) {
-        throw new Error("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
+      if (charCode > 0xff) {
+        throw new Error(
+          "'btoa' failed: The string to be encoded contains characters outside of the Latin1 range."
+        );
       }
 
-      block = block << 8 | charCode;
+      block = (block << 8) | charCode;
     }
 
     return output;
   },
 
-  atob: (input) => {
-    let str = input.replace(/=+$/, '');
-    let output = '';
+  atob: input => {
+    let str = input.replace(/=+$/, "");
+    let output = "";
 
     if (str.length % 4 == 1) {
-      throw new Error("'atob' failed: The string to be decoded is not correctly encoded.");
+      throw new Error(
+        "'atob' failed: The string to be decoded is not correctly encoded."
+      );
     }
-    for (let bc = 0, bs = 0, buffer, i = 0;
-      buffer = str.charAt(i++);
-
-      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
-        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+    for (
+      let bc = 0, bs = 0, buffer, i = 0;
+      (buffer = str.charAt(i++));
+      ~buffer && ((bs = bc % 4 ? bs * 64 + buffer : buffer), bc++ % 4)
+        ? (output += String.fromCharCode(255 & (bs >> ((-2 * bc) & 6))))
+        : 0
     ) {
       buffer = chars.indexOf(buffer);
     }
