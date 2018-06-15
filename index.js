f100f868271b1fc29df72c10b7469945f09ee6f5
Sang <hoang84119@gmail.com>
2018-06-15 15:22:57 +0700

    test

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-<<<<<<< HEAD
 import MediaToDetail from './src/navigations/MediaToDetail'
 AppRegistry.registerComponent('App', () => App);
-=======
-import PostTo from './src/navigations/PostTo'
-AppRegistry.registerComponent('App', () => PostTo);
->>>>>>> 202775ca96f9941ecfec0e5797556a708c5e38d0
 console.disableYellowBox = true;
