70fe95aef9157ef884defd9fd8c7ff7025d1f45d
Sang <hoang84119@gmail.com>
2018-07-12 10:25:59 +0700

    ok

--- src/components/Post/EditPost.js
+++ src/components/Post/EditPost.js
@@ -101,7 +101,7 @@ class EditPost extends Component {
             style={myStyle.richText}
             initialTitleHTML={this.state.noidung.title.rendered}
             initialContentHTML={this.state.noidung.content.rendered.replace(
-              "http://localhost",
+              "http://localhost/thuctap",
               API.getURL()
             )}
           />
@@ -230,7 +230,7 @@ class EditPost extends Component {
     };
     await API.Image.UploadImage(file).then(pathImage => {
       if (pathImage != "") {
-        pathImage = pathImage.replace("http://localhost", API.getURL());
+        pathImage = pathImage.replace("http://localhost/thuctap", API.getURL());
         this.richtext.insertImage({ src: pathImage });
       }
     });
