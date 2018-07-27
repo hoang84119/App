b5a655a04fbac17a1dea854ea0fa04d91e76bbf5
huyhieu127 <id.huyhieu@gmail.com>
2018-07-27 14:40:26 +0700

    report v1

--- src/components/Statistical.js
+++ src/components/Statistical.js
@@ -5,7 +5,9 @@ import {
   Text,
   StatusBar,
   StyleSheet,
-  FlatList
+  FlatList,
+  ImageBackground,
+  ActivityIndicator,
 } from "react-native";
 import Feather from "react-native-vector-icons/Feather";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -61,86 +63,90 @@ class Statistical extends Component {
         <View style={myStyle.cardItem}>
           {/* Buttons */}
 
-          <View style={{ flexDirection: "row", justifyContent: "center" }}>
+          <View style={{ flexDirection: "row", justifyContent: "center" , alignItems:"center"}}>
             <TouchableOpacity
               onPress={() => {
                 this.props.navigation.navigate("Post");
               }}
-              style={myStyle.buttons}
+              style={[myStyle.buttons,{borderColor: "#14d160"}]}
             >
               <Ionicons
-                  name={"ios-paper-outline"}
+                  name={"ios-paper"}
                   size={40}
-                  color={"#0ABFBC"}
+                  color={"#14d160"}
                 />
-              <Text style={myStyle.textSoLuong}>{this.state.posts}</Text>
+              <Text style={[myStyle.textSoLuong,{color: "#14d160"}]}>{this.state.posts}</Text>
               <Text style={{ fontSize: 12}}>Bài viết</Text>
             </TouchableOpacity>
             <TouchableOpacity
               onPress={() => {
                 this.props.navigation.navigate("Page");
               }}
-              style={myStyle.buttons}
+              style={[myStyle.buttons,{borderColor: "#fe5605"}]}
             >
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Ionicons
-                  name={"ios-book-outline"}
-                  size={30}
-                  color={"#0ABFBC"}
+            <Ionicons
+                  name={"ios-book"}
+                  size={40}
+                  color={"#fe5605"}
                 />
-                <Text style={myStyle.textSoLuong}>{this.state.pages}</Text>
-              </View>
-              <Text style={{ fontSize: 15, marginTop: 5 }}>Trang</Text>
+                <Text style={[myStyle.textSoLuong,{color: "#fe5605"}]}>{this.state.pages}</Text>
+            <Text style={{ fontSize: 12 }}>Trang</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={myStyle.buttons}>
-              <View style={{ flexDirection: "row", alignItems: "center" }}>
-                <Ionicons
-                  name={"ios-chatboxes-outline"}
-                  size={30}
-                  color={"#0ABFBC"}
+            <TouchableOpacity style={[myStyle.buttons,{borderColor: "#d213e8"}]}>
+              <Ionicons
+                  name={"ios-chatboxes"}
+                  size={40}
+                  color={"#d213e8"}
                 />
-                <Text style={myStyle.textSoLuong}>{this.state.comments}</Text>
-              </View>
-              <Text style={{ fontSize: 15, marginTop: 5 }}>Bình luận</Text>
+              <Text style={[myStyle.textSoLuong,{color: "#d213e8"}]}>{this.state.comments}</Text>
+              <Text style={{ fontSize: 12}}>Bình luận</Text>
             </TouchableOpacity>
           </View>
 
           {/* So luong khach */}
-
+          <View style={{flex: 1 , flexDirection:"row", justifyContent:"center"}}>
           <View
             style={{
-              marginLeft: 10,
+              margin: 10,
               flexDirection: "row",
               alignItems: "center"
             }}
           >
-            <Ionicons name={"ios-eye-outline"} size={30} color={"#0ABFBC"} />
-            <Text style={myStyle.text}>Tổng số người xem: {this.state.visitor}</Text>
+            <Ionicons name={"ios-eye"} size={30} color={"#0ABFBC"} />
+            <Text style={myStyle.text}>{this.state.visitor} đang xem</Text>
           </View>
           <View
             style={{
-              marginLeft: 10,
+              margin: 10,
               flexDirection: "row",
               alignItems: "center"
             }}
           >
-            <Ionicons name={"ios-people-outline"} size={30} color={"#0ABFBC"} />
-            <Text style={myStyle.text}>Tổng số khách viếng thăm: {this.state.visit}</Text>
+            <Ionicons name={"ios-people"} size={30} color={"#0ABFBC"} />
+            <Text style={myStyle.text}>{this.state.visit} lượt truy cập</Text>
+          </View>
           </View>
         </View>
 
         {/* Bài viết */}
         <View style={myStyle.cardItem}>
-          <Text style={myStyle.text}>Bài viết mới</Text>
+        <ImageBackground style={{padding: 5}} source={require("../image/tag/img5.jpg")}>
+          <Text style={{color: "#fff", fontSize: 16, paddingLeft: 5}}>Bài viết mới</Text>
+        </ImageBackground>
           <FlatList
-            data={this.state.dataPosts}
-            refreshing={this.state.refreshingPost}
-            keyExtractor={(item, index) => item.id.toString()}
-            renderItem={this._renderPost}
-          />
+              data={this.state.dataPosts}
+              refreshing={this.state.refreshingPost}
+              keyExtractor={(item, index) => item.id.toString()}
+              renderItem={this._renderPost}
+            />
         </View>
         <View style={myStyle.cardItem}>
-          <Text style={myStyle.text}>Bình luận mới</Text>
+          <ImageBackground style={{padding: 5}} source={require("../image/tag/img5.jpg")}>
+            <Text style={{color: "#fff", fontSize: 16, paddingLeft: 5}}>Bình luận mới</Text>
+          </ImageBackground>
+          <View>
+
+          </View>
         </View>
       </View>
     );
@@ -179,7 +185,7 @@ class Statistical extends Component {
 
   _renderPost = ({ item }) => (
     <TouchableOpacity style={{ flexDirection: "row" }}>
-      <Text style={myStyle.text}>{this._getDate(item.modified_gmt)}</Text>
+      <Text style={myStyle.text}>{this._getDate(item.modified_gmt)}    | </Text>
       <HTML
         html={`<span style="fontSize:16">${
           item.title.rendered
@@ -220,26 +226,34 @@ const myStyle = StyleSheet.create({
   },
   title: { fontSize: 20, color: "#fff", fontWeight: "500", marginLeft: 5 },
   cardItem: {
+    flex: 1,
     flexDirection: "column",
     margin: 5,
-    //borderWidth: 1,
-    borderRadius: 7,
-    shadowColor: "#000",
-    shadowOffset: { width: 10, height: 10 },
-    shadowOpacity: 0.8,
-    elevation: 1.5
+    borderWidth: 1,
+    borderRadius: 5,
+    overflow: "hidden",
+    borderColor: "#d0d0d0"
+    // shadowColor: "#000",
+    // shadowOffset: { width: 10, height: 10 },
+    // shadowOpacity: 0.8,
+    // elevation: 1.5
   },
   buttons: {
-    width: 100,
-    height: 100,
-    borderColor: "#c0c0c0",
-    borderWidth: 1,
-    borderRadius: 50,
+    padding: 15,
+    width: 110,
+    height: 110,
+    //borderColor: "#0ABFBC",
+    borderWidth: 2.5,
+    borderRadius: 1000,
     justifyContent: "center",
     alignItems: "center",
-    margin: 10
+    margin: 10,
+    // shadowColor: "blue",
+    // shadowOffset: { width: 0, height: 0 },
+    // shadowOpacity: 1,
+    // elevation: 3
   },
-  textSoLuong: { color: "#000", fontSize: 20},
+  textSoLuong: { color: "#0ABFBC", fontSize: 20, fontWeight: "bold"},
   text: { color: "#000", fontSize: 16, marginLeft: 10, marginVertical: 5 }
 });
 
